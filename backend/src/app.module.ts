d379f41789f3143ac52796a65b7827ba4f2e7112
joej0el <jhoel.titirico@uab.edu.bo>
2026-06-07 18:18:53 -0400

    chore(app): register participants module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { envValidation } from './shared/config/env.validation';
 import { AuthModule } from './auth/auth.module';
+import { ParticipantsModule } from './participants/participants.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { ChatModule } from './chat/chat.module';
 import { DatabaseModule } from './database/database.module';
@@ -17,6 +18,7 @@ import { DatabaseModule } from './database/database.module';
     DatabaseModule,
     AuthModule,
     RoomsModule,
+    ParticipantsModule,
     ChatModule,
 
 
